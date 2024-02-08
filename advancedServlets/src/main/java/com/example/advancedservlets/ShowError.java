9a1120d79c3fa2b1057904b7aae66a03553f8193
CK-EmbPro <kennydebrice2@gmail.com>
2024-02-08 22:24:47 +0200

    Send response errors and status codes in servlets

---
+++ advancedServlets/src/main/java/com/example/advancedservlets/ShowError.java
@@ -0,0 +1,19 @@
+package com.example.advancedservlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import jakarta.servlet.*;
+import java.io.*;
+import java.util.*;
+@WebServlet(name = "showError", value = "/showerror")
+public class ShowError extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException{
+        response.sendError();
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        doGet(request, response);
+    }
+
+}
